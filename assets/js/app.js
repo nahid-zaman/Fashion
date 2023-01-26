c53ae79d96a20a6e8b4fd10eb37a1a2601014445
Nahiduzzaman <nahidzaman.nahid@gmail.com>
2023-01-26 16:35:32 +0600

    all section completed

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 5,
+    spaceBetween: 100,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
